c937b5b477932b71bba0f6df244926e84f73d34a
Matthew Timm <matttimm1@aol.com>
2020-07-09 07:47:02 -0600

    level ending now has star animation.

--- Assets/Scripts/LevelEnder.cs
+++ Assets/Scripts/LevelEnder.cs
@@ -6,11 +6,35 @@ using UnityEngine.SceneManagement;
 public class LevelEnder : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    Animator animator;
+    bool animationFinished= false;
+    bool accessor = false;
+    void Awake(){
+        animator = gameObject.GetComponent<Animator>();
+        animator.SetBool("Collected", false);
+    }
     void OnTriggerEnter2D(Collider2D col){
         
         gameObject.GetComponent<BoxCollider2D>().enabled = false;
-        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        animator.SetBool("Collected", true);
         Invoke("nextLevel", 3f);
+    }
+
+    void Update(){
+       
+
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("StarCelebration")){
+            animationFinished = false;
+            accessor = true;
+            animator.SetBool("Collected", false);
+        }else if (accessor){
+            animationFinished = true;
+        }
+
+         if(animationFinished){
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
     }
 
     void nextLevel(){
